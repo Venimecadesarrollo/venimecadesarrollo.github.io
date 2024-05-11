f8b0abf7d550d368922065fb6619f3b4d5a6b453
Antonio <vila.antoniojose@gmail.com>
2024-05-11 14:52:16 -0400

    submenu info terrabox functional

--- main.js
+++ main.js
@@ -85,24 +85,102 @@ document.addEventListener("DOMContentLoaded", function () {
   setupSectionObserver();
   transitionInClickMenu();
   EventClickDownButton();
-  document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        const currentActive = document.querySelector('.content-section.active');
-        if (currentActive) {
-            currentActive.classList.remove('active');
-        }
+  document.querySelectorAll(".nav-link").forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      var currentComponent = document.querySelector(".content-section");
 
-        const newActiveId = 'content-' + this.id; // Este ID corresponde al contenido relacionado
-        console.log('newActiveId', newActiveId)
-        const newActive = document.getElementById(newActiveId);
-        if (newActive) {
-            newActive.classList.add('active');
-        }
+      if (currentComponent) {
+        removeInfoTerrabox();
+      }
+
+      addInfoTerrabox(this.id);
+    
     });
+  });
+
+  
 });
 
-});
+var componentWhatIs = `
+<div id="content-que-es" class="content-section">
+  <p>
+    TerraBox es esencial por su capacidad de mejorar 
+    significativamente la gestión de residuos, disminuyendo el impacto
+    ambiental y fomentando la conciencia ecológica. Este proyecto
+    innovador no solo utiliza tecnología avanzada para optimizar
+    el
+    reciclaje, sino que también involucra a la comunidad através de un
+    sistema de incentivos, ofreciendo beneficios económicos y
+    adaptándos a diferentes entornos urbanos y rurales, políticas
+  locales, y necesidad especificas de gestión de residuos.
+  </p>
+</div>
+`;
+  
+var componentApproach = `
+<div id="content-enfoque-y-abordaje" class="content-section">
+  <p>
+    Optimizar la recolección y gestión de materiales reciclables, con
+    el fin de mejorar así la eficiencia en la reutilización urbana y
+    fom entar la participación ciudadana. Metodología El proyecto se
+    enfocará en el diseño, implementación y evaluación del TerraBox
+    mediante técnicas de ingeniería y análisis de sostenibilidad. con
+    pruebas piloto y recopilación de datos para medir su impacto y
+    eficaci
+  </p>
+</div>`;
+
+var componentProposal = `
+<div id="content-propuesta" class="content-section">
+  <ul id="proposal-list">
+    <li>
+      Puerto la Cruz
+      <ul>
+        <li>Paseo Colón</li>
+      </ul>
+    </li>
+    <li>
+      Barcelona
+      <ul>
+        <li>Plaza Bolívar</li>
+      </ul>
+    </li>
+    <li>
+      Lechería
+      <ul>
+        <li>Comedor Popular</li>
+        <li>Turístico</li>
+        <li>Alberto Lovera</li>
+      </ul>
+    </li>
+  </ul>
+</div>
+`;
+
+var containerInfo = document.querySelector("#product-content");
+
+function addInfoTerrabox(concept) {
+  var currentComponent = ''
+  if (concept === "what-is") {
+    containerInfo.innerHTML = componentWhatIs;
+    currentComponent = document.querySelector(".content-section");
+  }else if (concept === "approach") {
+    containerInfo.innerHTML = componentApproach;
+    currentComponent = document.querySelector(".content-section");
+  }else if (concept === "proposal") {
+    containerInfo.innerHTML = componentProposal;
+    currentComponent = document.querySelector(".content-section");
+  }
+
+  setTimeout(() => {
+    currentComponent.classList.toggle("active");
+  }, 100);
+}
+
+function removeInfoTerrabox() {
+  containerInfo.innerHTML = "";
+}
 
 function transitionInClickMenu() {
   const menuLinks = document.querySelectorAll("nav a");
